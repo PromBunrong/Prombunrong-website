42dcb122d139ccdfd532c8db9261d8a313cdb1b3
PromBunrong <prom.bunrong543@gmail.com>
2024-05-18 10:47:36 +0700

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,19 @@
+
+var typed = new Typed(".text", {
+    strings: ["Data Management" , "Data Analysis" , "Data Engineering" , "Data Scientist"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
